a9566c86beac5651275c99b40c4941157b3179e9
Erin Isabel <eanand@andrew.cmu.edu>
2021-11-18 13:32:31 -0500

    added splash screen

--- Term Project Code - PyKeys.py
+++ Term Project Code - PyKeys.py
@@ -4,23 +4,37 @@ import musicalbeeps
 #needed to play sounds
 player = musicalbeeps.Player(volume = 0.3, mute_output = False)
 
-def splashScreenMode_redrawAll(app, canvas):
-    font = 'Arial 26 bold'
-    canvas.create_text(app.width/2, 150, text='Piano Tones', font='Arial 80 bold', fill = 'red')
-    canvas.create_image(200, 70, image=ImageTk.PhotoImage(app.image1))
 
-def splashScreenMode_keyPressed(app, event):
-	app.mode = 'gameMode'
-
+##### SPLASH SCREEN ######
+def ScreenMode_redrawAll(app, canvas):
+	(canvas.create_text(app.width/2,app.height/3.34,font=
+	'Times 20 bold', text= "Press 'a' for the game mode", fill = 'blue'))
+	(canvas.create_text(app.width/2,app.height/3,font=
+	'Times 20 bold', text= "Press 'f' for the freestyle mode", fill = 'blue'))
+	(canvas.create_text(app.width/2,app.height/3.7,font=
+	'Times 20 bold', text= "Press 'c' for the composition mode", fill = 'blue'))
+	(canvas.create_text(app.width/2, app.height/6, text='PyKeys', 
+	font='Times 80 bold', fill = 'black'))
+	(canvas.create_image(app.width/2, app.height*2.5/4,
+	 image=ImageTk.PhotoImage(app.image2)))
+	
+def ScreenMode_keyPressed(app, event):
+	if event.key == 'a':
+		app.mode = 'gameMode'
+	if event.key == 'f':
+		app.mode = 'freeStyle'
+	if event.key == 'c':
+		app.mode = 'composition'
 
 def appStarted(app):
 	#initalizes everything
-	app.mode = 'splashScreenMode'
+	app.mode = 'composition'
+	app.mode = 'ScreenMode'
 	app.label = 'Piano! :P'
 	app.song = ' '
 	app.keys = []
 	app.flatsandsharps = []
-	app.image1 = app.loadImage('IMG_0104.jpg')
+	app.image1 = app.loadImage('Snake Logo.jpg')
 	#saves x0,y0,x1,y1 values
 	for x in range(16):
 		(x0, y0, x1, y1) = getKeyBounds(app, x)
@@ -56,8 +70,11 @@ def appStarted(app):
 	app.num = 0
 	app.score = 0
 	app.gameOver = False
-	app.image1 = app.loadImage('IMG_0104.jpg')
-
+	app.image1 = app.loadImage('Snake Logo.jpg')
+	app.image2 = app.scaleImage(app.image1, 1/3)
+	app.notePlayed = ""
+	
+########## GAME MODE ###########
 def gameMode_keyPressed(app, event):
 	if event.key == 'k':
 		for x in range(len(app.notes)):
@@ -68,14 +85,17 @@ def gameMode_mousePressed(app, event):
 	x = event.x
 	y = event.y
 	flag = False
-	letters = ['C3','D3','E3','F3','G3','A3','B3','C4','D4','E4','F4','G4','A4','B4','C5','D5']
+	letters = (['C3','D3','E3','F3','G3','A3','B3',
+				'C4','D4','E4','F4','G4','A4','B4',
+				'C5','D5'])
 	for sharp in app.flatsandsharps:
 		x00 = sharp[0]
 		y00 = sharp[1]
 		x10 = sharp[2]
 		y10 = sharp[3]
 		if x > x00 and x < x10 and y > y00 and y < y10:
-			flatsandsharps = ['C3#','E3b','F3#','G3#','A3#','C4#','D4#','F4#','G4#','A4#','C5#','D5#']
+			flatsandsharps = (['C3#','E3b','F3#','G3#','A3#','C4#',
+							   'D4#','F4#','G4#','A4#','C5#','D5#'])
 			index = app.flatsandsharps.index(sharp)
 			player.play_note(flatsandsharps[index], 0.4)
 			flag = True
@@ -108,6 +128,7 @@ def getKeyBounds(app, col):
 	y0 = app.height //1.5
 	y1 =  app.height
 	return (x0, y0, x1, y1)
+
 #draws keys
 def drawKey(app,canvas):
 	for x in range(16):
@@ -116,15 +137,18 @@ def drawKey(app,canvas):
 
 #draws flats and sharps
 def drawFlatsandSharps(app,canvas):
-	letters = ['C3','D3','E3','F3','G3','A3','B3','C4','D4','E4','F4','G4','A4','B4','C5','D5']
+	letters = (['C3','D3','E3','F3','G3','A3','B3','C4'
+	           ,'D4','E4','F4','G4','A4','B4','C5','D5'])
 	for x in range(16):
 		(x0, y0, x1, y1) = getKeyBounds(app, x)
 	width = x1-x0
 	for x in range(16):
 		(x0, y0, x1, y1) = getKeyBounds(app, x)
 		if x!= 2 and x!=6 and x!= 9 and x!= 13:
-			canvas.create_rectangle(x0 + (3*(width)/4),app.height //1.5, x1+(width//3), 3*app.height/3.5, fill = 'black')
-		canvas.create_text((x0+x1)//2,app.height*0.96,text= f'{letters[x]}', font = 'Arial 15 bold', fill = 'black')
+			canvas.create_rectangle(x0 + (3*(width)/4),app.height //1.5, 
+			x1+(width//3), 3*app.height/3.5, fill = 'black')
+		(canvas.create_text((x0+x1)//2,app.height*0.96,
+		text= f'{letters[x]}', font = 'Arial 15 bold', fill = 'black'))
 
 #draws the bubble in the place of the note to be played
 def drawFallingBubble(app, canvas):
@@ -133,24 +157,23 @@ def drawFallingBubble(app, canvas):
 		note, x0, y0, x1, y1 = app.bubbles[app.num]
 		canvas.create_oval(x0,app.bubblesy,x1,app.bubblesy1, fill = 'green')
 
-# working on this
 # target: to make the bubble fall down the screen in the correct order
 def moveFallingBubble(app, drow):
 	if app.bubblesy < app.height or app.bubblesy1 < app.height:																
 		app.bubblesy += drow
 		app.bubblesy1 += drow
 
-
 def gameMode_timerFired(app):
 	moveFallingBubble(app, +25)
-	if (app.num < len(app.bubbles)-1) and (app.bubblesy > app.height or app.bubblesy1 > app.height):
+	if ((app.num < len(app.bubbles)-1) and (app.bubblesy > app.height
+	 			or app.bubblesy1 > app.height)):
 		app.num += 1
 		app.bubblesy = 0
 		app.bubblesy1 = app.height/10
 	if app.num == len(app.bubbles)-1:
 		app.gameOver = True
 	
-# working on this- check if the bubble is still in the dimensions of the screen
+# check if the bubble is still in the dimensions of the screen
 # if not, remove from app.bubbles
 def BubbleonScreen(app):
 	for item in app.bubbles:
@@ -176,9 +199,60 @@ def gameMode_redrawAll(app, canvas):
 	drawPiano(app, canvas)
 	#draws all the bubbles (presently)
 	drawFallingBubble(app,canvas)
-	canvas.create_text(app.width/2, app.height/4, text = f"SCORE = {app.score}")
+	(canvas.create_text(app.width/2, app.height/4, 
+	font = 'Times 20 bold', text = f"SCORE = {app.score}"))
 	if app.gameOver == True:
-		canvas.create_text(app.width/2, app.height/8,text=f'Final Score = {app.score}', font = 'Arial 30 bold', fill = 'black')
+		(canvas.create_text(app.width/2, app.height/8,text=
+		f'Final Score = {app.score}', font = 'Times 50 bold', fill = 'blue'))
+		(canvas.create_text(app.width/2, app.height/5.5,text=
+		"Press 'k' to hear what it is supposed to sound like!", font = 'Times 20 bold', fill = 'red'))
+
+###### FREESTYLE #######
+def freeStyle_mousePressed(app, event):
+	#gets x and y coordinate from key click
+	x = event.x
+	y = event.y
+	flag = False
+	letters = (['C3','D3','E3','F3','G3','A3','B3',
+				'C4','D4','E4','F4','G4','A4','B4',
+				'C5','D5'])
+	for sharp in app.flatsandsharps:
+		x00 = sharp[0]
+		y00 = sharp[1]
+		x10 = sharp[2]
+		y10 = sharp[3]
+		if x > x00 and x < x10 and y > y00 and y < y10:
+			flatsandsharps = (['C3#','E3b','F3#','G3#','A3#','C4#',
+							   'D4#','F4#','G4#','A4#','C5#','D5#'])
+			index = app.flatsandsharps.index(sharp)
+			player.play_note(flatsandsharps[index], 0.4)
+			app.notePlayed = flatsandsharps[index]
+			flag = True
+			break
+
+	for key in app.keys:
+		x0 = key[0]
+		y0 = key[1]
+		x1 = key[2]
+		y1 = key[3]
+		for sharp in app.flatsandsharps:
+			x00 = sharp[0]
+			y00 = sharp[1]
+			x10 = sharp[2]
+			y10 = sharp[3]
+			if flag != True:
+				if (x > x0 and x < x1 and y > y0 and y < y1):
+					index = app.keys.index(key)
+					player.play_note(letters[index], 0.36)
+					app.notePlayed = letters[index]
+					break
+
+def freeStyle_redrawAll(app, canvas):
+	drawPiano(app, canvas)
+	canvas.create_text(app.width/2, app.height/3, text= f"You just pressed: {app.notePlayed}",font = 'Times 50 bold', fill = 'blue')
+
+##### COMPOSITION #####
+
 
 #run app
 def playPiano():
